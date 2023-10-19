d1a4afa07196fe5ed1fad16050483b1f39cd9bd6
AaronSirico <aarronsirico@gmail.com>
2023-10-18 23:07:19 -0500

    Pulled from Produce text

---
+++ DataDesignSQL/Product.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataDesignSQL
+{
+    public class Product
+    {
+        public int ID { get; set; }
+
+        public string Name { get; set; }
+
+        public string Location { get; set; }
+
+        public float Price { get; set; }
+       
+        
+        public string UoM { get; set; }
+
+        public string SellByDate { get; set; }
+
+
+        public Product()
+        {
+
+        }
+        public Product(int id, string name, string location, float price, string uoM, string sellByDate )
+        {
+
+            ID = id;
+            Name = name;
+            Location = location;
+            try
+            {
+                Price = price;
+            }
+            catch (Exception)
+            {
+                price = 0;
+            }
+            UoM = uoM;
+            SellByDate = sellByDate;
+
+
+        }
+
+        public string NewLocation(ref string Location)
+        {
+            if (Location.Contains("F"))
+            {
+               Location = Location.Replace("F", "Z");
+            }
+            else if (Location.Contains("A"))
+            {
+               Location = Location.Replace("A", "Z");
+            }
+            return Location;
+        }
+
+        public string SellDate(ref string SellByDate)
+        {
+            if (SellByDate.Contains("2022"))
+            {
+                SellByDate = null;
+            }
+            return SellByDate;
+        }
+
+        public float UpdatePrice(ref float Price)
+        {
+            float NewPrice;
+            if (Price != null)
+            {
+                NewPrice = Price;
+                Price = Price + 1;
+                return NewPrice;
+            }
+            return 0;
+
+        }
+    }
+}
--- DataDesignSQL/Program.cs
+++ DataDesignSQL/Program.cs
@@ -4,24 +4,105 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Data.SqlClient;
+using System.IO;
+using static System.Net.WebRequestMethods;
 
 namespace DataDesignSQL
 {
     internal class Program
     {
+
         static void Main(string[] args)
         {
+            Product p = new Product();  
+           
+            var Path = ($"{Directory.GetCurrentDirectory()}\\Files");
+            string File = $"{Path}\\Produce.txt";
+            List<Product>Products = new List<Product>();
+            int i = 0;
+
+
+            using (StreamReader sr = new StreamReader(File))
+            {  
+                var line = sr.ReadLine();
+                while (line != null)
+                {
+                   if (i < 1)
+                    {
+                        i++;
+                        line = sr.ReadLine();
+                    }
+                   else
+                    {
+                        var data = line.Split('|').ToList();
+                        data = data.ToList();
+                        Product Prod = new Product(1, data[0], data[1], float.Parse(data[2]), data[3], data[4]);
+                        if (data[4].Contains("2023") || data[4].Contains("2024"))
+                        {
+                            Products.Add(Prod);
+                        }
+                       
+                        line = sr.ReadLine();
+                    }
+                    
+                }
+            }
+            Update up = new Update();
+
+            
+
+            foreach (Product prod in Products)
+            {
+                //New Location
+                string NewLocation;
+                NewLocation = prod.Location;
+                p.NewLocation(ref NewLocation);
+                prod.Location = NewLocation;
+            }
+
+
+            foreach (Product prod in Products)
+            {
+                //New Location
+                float NewPrice;
+                NewPrice = prod.Price;
+                p.UpdatePrice(ref NewPrice);
+                prod.Price = NewPrice;
+                
+
+            }
+
+
+
             SqlConnectionStringBuilder mySqlConBldr = new SqlConnectionStringBuilder();
             mySqlConBldr["server"] = @"(localdb)\MSSQLLocalDB";
             mySqlConBldr["Trusted_Connection"] = true;
             mySqlConBldr["Integrated Security"] = "SSPI";
             mySqlConBldr["Initial Catalog"] = "PROG260FA23";
+
             string sqlConStr = mySqlConBldr.ToString();
+
+            foreach (Product prod in Products)
+            {
+                using (SqlConnection conn = new SqlConnection(sqlConStr))
+                {
+                    
+                    conn.Open();
+                    string inlineSQL = $@"INSERT [dbo].[Produce] ([Name], [Location], [Price], [UoM], [Sell_by_Date]) VALUES ('{prod.Name}', '{prod.Location}', '{prod.Price}', '{prod.UoM}', '{prod.SellByDate}')";
+                    using (var command = new SqlCommand(inlineSQL, conn))
+                    {
+                        var query = command.ExecuteNonQuery();
+                        Console.ReadLine();
+                    }
+                    conn.Close();
+
+                }
+            }
 
             using (SqlConnection conn = new SqlConnection(sqlConStr))
             {
                 conn.Open();
-                string inlineSQL = @"Select * from Game";
+                string inlineSQL = @"Select * from Produce";
                 using (var command = new SqlCommand(inlineSQL, conn))
                 {
                     var reader = command.ExecuteReader();
@@ -36,19 +117,6 @@ namespace DataDesignSQL
 
                 }
                 conn.Close();
-            }
-
-            using (SqlConnection conn = new SqlConnection(sqlConStr))
-            {
-                conn.Open();
-                string inlineSQL = @"INSERT [dbo].[Game] ([Name], [Publisher], [Release_Date], [Sold], [Rating]) VALUES ('The Secret of Monkey Island', 'Lucasfilm Games', '10-01-1990', NULL, 94)";
-                using (var command = new SqlCommand(inlineSQL, conn))
-                {
-                    var query = command.ExecuteNonQuery();
-                    Console.ReadLine();
-                }
-                conn.Close();
-
             }
         }
     }
